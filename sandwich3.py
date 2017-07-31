d078c717bc33cd9f60041503e496ca3422d3cfe9
Lauren Aldrich <lauren.e.aldrich@gmail.com>
2017-07-31 01:05:00 +0000

    updated program

--- sandwich3.py
+++ sandwich3.py
@@ -3,15 +3,17 @@ def get_input():
     return input("How many sandwiches can you fit in your mouth? ")
 
 def you_lose(count, bro):
+    if count <0 :
+        return
     if count <= 4:
         print "You said, ", count, "but my brother can eat ", bro
     else:
         print "You will get sick if you eat", count, "sandwiches!"
-    print "Therefore, you lost!!! :("
-
-count = get_input()
-my_brother = count + 1
-you_lose(count, my_brother)
 
 
+count = 0 
+while count >= 0 :
+    count = get_input()
+    you_lose(count, count+1)
 
+print "You should really try harder. Even a baby can eat zero sandwiches!"
